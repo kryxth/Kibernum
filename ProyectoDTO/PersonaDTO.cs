e304c9873d28a0a8bc7ecac28b639a8a911b6d33
Cristhian Burgos <cristhian.burgos@gmail.com>
2022-11-04 11:27:24 -0500

    Proyecto DTO

---
+++ ProyectoDTO/PersonaDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoDTO
+{
+    public class PersonaDTO
+    {
+        public int IdPersona { get; set; }
+        public string Documento { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string NumerosTelefonicos { get; set; }
+        public string CorreosElectronicos { get; set; }
+        public string DirecionesFisicas { get; set; }
+    }
+}
